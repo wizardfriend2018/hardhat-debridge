2719898385a78e75330eeb65944f9975c3d9ece4
wizardfriend2018 <44982576+wizardfriend2018@users.noreply.github.com>
2026-01-18 13:20:22 -0500

    Update MockSignatureVerifier.sol

--- contracts/MockSignatureVerifier.sol
+++ contracts/MockSignatureVerifier.sol
@@ -1,18 +1,15 @@
 //SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.7;
 
-import "@debridge-finance/contracts/contracts/interfaces/ISignatureVerifier.sol";
-
-contract MockSignatureVerifier is ISignatureVerifier {
+import "@debridge-
     function submit(
         bytes32 /*_submissionId*/,
         bytes memory /*_signatures*/,
-        uint8 /*_excessConfirmations*/
-    ) external override {/*_*/}
+      
 }[ ] enableAutoHarvest
-[ ] disableAutoHarvest
+[ ]
 [ ] enableMultiProtocol
-[ ] disableMultiProtocol
+[ 
 [ ] routeToAave
 [ ] routeToYearn
 [ ] skimProfits
